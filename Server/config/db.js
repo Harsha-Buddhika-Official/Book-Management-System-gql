b05fcd12a042bbe94586df7fbdf2229c54be73bd
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-10-05 14:58:11 +0530

    Update db.js

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017/";
+const uri = process.env.MONGO_URI;
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(uri);
