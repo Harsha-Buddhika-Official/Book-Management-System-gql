ab073cec5319d05f05d3805ace458b3287efd654
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-10-10 15:51:18 +0530

    Update mutation.js

--- Client/src/graphql/mutation.js
+++ Client/src/graphql/mutation.js
@@ -1,5 +1,33 @@
 import { gql } from "@apollo/client";
 
+export const ADD_BOOK = gql`
+  mutation CreateBook {
+    createBook {
+      title
+      author
+      year
+      genre
+      image
+      description
+      language
+    }
+  }
+`;
+
+export const UPDATE_BOOK = gql`
+  mutation Mutation($updateBookId: ID!) {
+    updateBook(id: $updateBookId) {
+      title
+      author
+      year
+      genre
+      image
+      description
+      language
+    }
+  }
+`;
+
 export const DELETE_BOOK = gql`
   mutation Mutation($deleteBookId: ID!) {
     deleteBook(id: $deleteBookId) {
